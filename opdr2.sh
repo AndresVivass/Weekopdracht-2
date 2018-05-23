1a9933a5f7b8831e38469106cd4ac5a15c825416
Unknown <a.m.vivas.rodriguez@st.hanze.nl>
2018-05-24 00:08:24 +0200

    successful in moving files to directory

--- opdr2.sh
+++ opdr2.sh
@@ -1,20 +1,25 @@
-#!bin/bash
+#!/bin/bash
 
 #### Weekopdracht 2 ####
-echo "========================="
+echo "=================================================="
 # Create: Month & Week folders on ~/Documents
 # ~/Documents/Month
 # ~/Documents/Week
 # The ~/Pictures directory should contain the 10 fotos 
 
-countPics=$(ls ~/Pictures | wc)	# a counter for the amount of pics in ~/Pictures
+countPics=(ls ~/Pictures | wc)	# a counter for the amount of pics in ~/Pictures
 
 
-# Move to Month directory
+# Move to Month directory ('cp' is being used)
 for i in ~/Pictures
 do
- cp ~/Pictures/* ~/Documents/Month
+# cp ~/Pictures/* ~/Documents/Month
 
- echo "You have successfully moved "+" $count_Pics "+"files."
-
+ echo 'You have successfully moved "$countPics" files.'
 done
+
+
+
+
+
+echo "=================================================="
