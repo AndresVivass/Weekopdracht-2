e1e03d6cf144850c3cb22ec86d2e8d0ef6e51d98
Unknown <a.m.vivas.rodriguez@st.hanze.nl>
2018-05-24 12:23:22 +0200

    Recent changes

--- opdr2.sh
+++ opdr2.sh
@@ -1,31 +1,57 @@
 #!/bin/bash
-
+#=========================================================================
 #### Weekopdracht 2 ####
-echo "=================================================="
+#=========================================================================
 # 2 parameteres (1- Foto's directory  2- Month/Week indication)
 # copy to a newly created directory (cp command)
 # Use For-loop
 # Verify if successfully copied
 #  - compare MD5sum of both pictures
 # Remove pictures from their original location
+#==========================================================================
 
 # Initialize variables
-photo_dir=(~/Pictures)		# Parameter 1: Photo directory
-indi_dir="Month"		# Parameter 2: Folder indication
+photo_dir=(~/Pictures/*)	# Parameter 1: Photo directory
+dest_dir=(~/Documents/'Month')	# Parameter 2: Folder indication (Month | Week)
 
 countPics=(ls ~/Pictures | wc)	# Extra: counter for the amount of pictures
 
-
-# Move to Month directory ('cp' is being used)
-for i in ~/Pictures
+# Move pictures to $indi_dir
+for i in ~/Pictures;
 do
-# cp ~/Pictures/* ~/Documents/Month
-
- echo 'You have successfully moved "$countPics" files.'
+  cp $photo_dir $dest_dir
 done
 
+if (..............);  # Confirm if files have been copied
+then
+  echo "You have successfully moved '${countPics}' files."
+
+else
+  echo "Copying Failed"
 
 
+
+#rm ~/Pictures/*
+
+
+
+
+
+
+echo ""
+
+
+
+
+echo -e "Please specify the folder to copy pictures to (Month|Week): \c"
+read word
+echo "======================================="
+echo "The chosen directory was: $word"
+echo "======================================="
+echo -e "Is this correct?[y/n]: \c"
+read answer
+
+echo "$answer"
 
 
 echo "=================================================="
