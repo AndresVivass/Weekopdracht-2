5f22b6d83193f6d8f6fcd5fcfde5da66d63077b7
Unknown <a.m.vivas.rodriguez@st.hanze.nl>
2018-05-29 17:11:44 +0200

    Changes

--- opdr2.sh
+++ opdr2.sh
@@ -1,28 +1,60 @@
 #!/bin/bash
 #=========================================================================
-#### Weekopdracht 2 ####
+#=== Weekopdracht 2 ===
 #=========================================================================
 # 2 parameteres (1- Foto's directory  2- Month/Week indication)			- Done
 # copy to a newly created directory (cp command)				- Done
 # Use For-loop									- Done
-# Verify if successfully copied							- Progress
-#  - compare both pictures
-# Remove pictures from their original location					- Progress
+# Verify if successfully copied							- Done
+#  - compare pictures (MD5)
+# Remove pictures from their original location					- Done
 #==========================================================================
 
-# Parameter(s)
-#-------------------
-# $1: Photo directory
-# $2: Folder indication (Month | Week)
+cd $1
+dir_1="$1"
 
-# Copy & Move photos to newly created directory ($2)
-if [[ -f "$1" || -d "$1" ]]; then
-  for pic in "$1"/.; do
-    cp -r $1 $2
-  done
+shopt -s nullglob
+shopt -s nocaseglob
+num=0
+for i in *.jpeg; do
+	i2=${i##*/}
+	if [[ $2 = "Month" ]]		# Directory: Month
+	then
+		date1=$(date -r $i +%B) # Creates sub dir. based on current month
+		mkdir -p Month
+	elif [[ $2 = "Week" ]]		# Directory: Week
+	then
+		date1=$(date -r $i +%W)	# creates sub dir. bases on current week
+		mkdir -p Week
+	else
+		echo "!ERROR!: Second variable must be Month or Week"
+	fi
+
+md5_check1=($(md5sum $i2))			# MD5 creation
+mkdir -p ./$2/$date1
+cp $i ./$2/$date1
+md5_check2=($(md5sum ./$2/$date1/$i2))
+
+	if [ "$md5_check1" == "$md5_check2" ]	# Verify files (MD5)
+	then
+		echo "hashes are correct"
+		rm -f $i
+	else
+		echo "$1 is not the same"
+	fi
+	((num++))
+done
+
+if [[ $num < 1 ]]		# Checks if directory is empty
+then
+	echo "================================="
+	echo " There were no files to sort"
+	echo "================================="
+else
+	echo "================================="
+	echo " $num files were sorted"
+	echo "================================="
 fi
 
-# Verify if successful then delete original picture location
-if [[ "$1" == "$2" ]]; then
-  echo "match!"
-fi
+shopt -u nocaseglob
+shopt -u nullglob
