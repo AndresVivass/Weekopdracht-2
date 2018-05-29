8917ffd8d67f31e9062f80b0f4cdf8d30d6fcb56
Unknown <a.m.vivas.rodriguez@st.hanze.nl>
2018-05-29 16:02:19 +0200

    New changes to text-GUI

--- textInterface.sh
+++ textInterface.sh
@@ -1,19 +1,32 @@
 #! /bin/bash
-
-
-
+#==================================================
+# testing a text interface for Linux - Week 2
+#
+#
+#==================================================
 echo "============================="
-echo "Rodriguez's Backup Tool"
+echo " Rodriguez's Backup Tool"
 echo "============================="
 echo -e "Please specify the folder to copy pictures to (Month|Week): \c"
 read word
+echo ""
 echo "---------------------------------------"
 echo "The chosen directory was: $word"
 echo "---------------------------------------"
 echo -e "Is this correct?[y/n]: \c"
 read answer
+echo ""
+echo "Progress:"
+echo "--------------"
 
-echo "$answer"
+if( {$answer} == 'y' ); then
+  echo "process 1................"
+  echo "process 2................"
+elif( {$answer} = 'n' ); then
+  echo "process Terminated.............."
+else
+  echo "!Error!: Invalid option, $answer"
+fi
 
 
-echo "=================================================="
+
